73b355f1a0cd89e6881cfaf77b7c83adcf0c524a
Ocks <milomgenova@gmail.com>
2025-03-22 12:24:42 -0400

    stored rands ready for testing

--- Assets/OcksTools/Systems/Misc/StoredRandom.cs
+++ Assets/OcksTools/Systems/Misc/StoredRandom.cs
@@ -1,24 +1,73 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class StoredRandom : MonoBehaviour
 {
+    public bool AutoSaveAndLoad = true;
     public Dictionary<string, OXRandStore> randoms = new Dictionary<string, OXRandStore>();
+
+    private void Awake()
+    {
+        if (AutoSaveAndLoad)
+        {
+            SaveSystem.SaveAllData.Append(SS_SaveRandData);
+            SaveSystem.LoadAllData.Append(SS_LoadRandData);
+        }
+    }
 
     public void DefineRandom(string name, int start)
     {
         if (randoms.ContainsKey(name))
         {
-            randoms[name] = new OXRandStore(start);
+            randoms[name] = new OXRandStore(start, name);
         }
         else
         {
-            randoms.Add(name, new OXRandStore(start));
+            randoms.Add(name, new OXRandStore(start, name));
         }
+    }
+    public void UnallocateRandom(string name)
+    {
+        randoms.Remove(name);
     }
 
     public System.Random GetRand(string a) { return randoms[a].Rand; }
+
+    public string ExportToString()
+    {
+        List<string> list = new List<string>();
+        foreach(var a in randoms)
+        {
+            list.Add(a.Value.ConvertToString());
+        }
+        return Converter.ListToString(list, "||");
+
+    }
+    public void ImportFromString(string aa)
+    {
+        randoms.Clear();
+        if (aa == "")
+        {
+            return;
+        }
+        List<string> list = Converter.StringToList(aa, "||");
+        foreach(var a in list)
+        {
+            var e = new OXRandStore(a);
+            randoms.Add(e.Name, e);
+        }
+    }
+
+    public void SS_SaveRandData(string dict)
+    {
+        SaveSystem.Instance.SetString("Randoms", ExportToString(), dict);
+    }
+    public void SS_LoadRandData(string dict)
+    {
+        ImportFromString(SaveSystem.Instance.GetString("Randoms", "", dict));
+    }
 
 
 }
@@ -28,15 +77,22 @@ public class OXRandStore
     public System.Random Rand;
     public int Hits = 0;
     public int Initial = 0;
-    public OXRandStore(int start)
+    public string Name = "";
+    public OXRandStore(int start, string name)
     {
         Rand = new System.Random(start);
         Initial = start;
+        Name = name;
+    }
+    public OXRandStore(string initaldata)
+    {
+        StringToData(initaldata);
     }
     public string ConvertToString()
     {
         return Converter.ListToString(new List<string>()
         {
+            Name,
             Hits.ToString(),
             Initial.ToString(),
         }, ";;");
@@ -44,8 +100,9 @@ public class OXRandStore
     public void StringToData(string data)
     {
         var dat = Converter.StringToList(data, ";;");
-        Hits = int.Parse(dat[0]);
-        Initial = int.Parse(dat[1]);
+        Name = dat[0];
+        Hits = int.Parse(dat[1]);
+        Initial = int.Parse(dat[2]);
         Rand = new System.Random(Initial);
         for(int i = 0; i < Hits; i++) // I cant think of a better way to do this lol
         {
