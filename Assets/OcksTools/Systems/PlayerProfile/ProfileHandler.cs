c106572e91f2c733154ea73ed79b030e6a1d9394
Ocks <milomgenova@gmail.com>
2025-07-01 23:51:27 -0400

    PPS

--- Assets/OcksTools/Systems/PlayerProfile/ProfileHandler.cs
+++ Assets/OcksTools/Systems/PlayerProfile/ProfileHandler.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class ProfileHandler : MonoBehaviour
 {
-    // This is an Experimental Class, mostly used to begin interacting with an idea I had about a universal profile, needs much work
 
+    public static string Username = "";
 
-    // Start is called before the first frame update
     void Awake()
     {
         SaveSystem.LoadAllData.Append(LockIn);
+        SaveSystem.SaveAllData.Append(LockOut);
     }
 
 
@@ -18,12 +18,21 @@ public class ProfileHandler : MonoBehaviour
     {
         var s = SaveSystem.Instance;
         s.GetDataFromFile("ox_profile");
-        if(s.GetString("Username", "", "ox_profile") == "")
+        if (s.GetString("Username", "", "ox_profile") == "")
         {
-            s.SetString("Username", $"Guest{RandomFunctions.CharPrepend(Random.Range(0,1000000).ToString(), 6, '0')}", "ox_profile");
+            s.SetString("Username", $"Guest{RandomFunctions.CharPrepend(Random.Range(0, 1000000).ToString(), 6, '0')}", "ox_profile");
         }
+        Username = s.GetString("Username", "", "ox_profile");
+        Console.Log("Logged In User: " + Username);
+    }
+    public void LockOut(string dict)
+    {
+        var s = SaveSystem.Instance;
+        if (Username == "") return;
 
-        
+        s.SetString("Username", Username, "ox_profile");
+
+        s.SaveDataToFile("ox_profile");
     }
 
 }
