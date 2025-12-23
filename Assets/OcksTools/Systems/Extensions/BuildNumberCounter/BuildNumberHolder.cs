586940ab59a4ff65c0429692b2d6b8d7fdfa16d9
Ocks <milomgenova@gmail.com>
2025-12-23 01:46:41 -0500

    as

--- Assets/OcksTools/Systems/Extensions/BuildNumberCounter/BuildNumberHolder.cs
+++ Assets/OcksTools/Systems/Extensions/BuildNumberCounter/BuildNumberHolder.cs
@@ -1,14 +1,20 @@
 using NaughtyAttributes;
 using UnityEngine;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 [CreateAssetMenu(fileName = "BuildNumberHolder", menuName = "Scriptable Objects/BuildNumberHolder")]
 public class BuildNumberHolder : ScriptableObject
 {
     public int BuildNumber;
-
+#if UNITY_EDITOR
     [Button]
     public void ResetBuildNum()
     {
         BuildNumber = 0;
+        EditorUtility.SetDirty(this);
+        AssetDatabase.SaveAssets();
     }
+#endif
 }
