a7febf94087a0333309588fe32cacbc940c9fd25
Ocks <milomgenova@gmail.com>
2026-01-31 16:20:39 -0500

    chess blud

--- Assets/OcksTools/Systems/Extensions/Chess/ChessEngine.cs
+++ Assets/OcksTools/Systems/Extensions/Chess/ChessEngine.cs
@@ -4,6 +4,48 @@ using static ChessEngine;
 
 public class ChessEngine
 {
+    public static BoardState SetUpDefaultMatch()
+    {
+        BoardState board = new Chess_DefaultBoard();
+
+        board.AddPiece(new Chess_Pawn().SetTeam(ChessTeam.White), new Vector2Int(0, 1));
+        board.AddPiece(new Chess_Pawn().SetTeam(ChessTeam.White), new Vector2Int(1, 1));
+        board.AddPiece(new Chess_Pawn().SetTeam(ChessTeam.White), new Vector2Int(2, 1));
+        board.AddPiece(new Chess_Pawn().SetTeam(ChessTeam.White), new Vector2Int(3, 1));
+        board.AddPiece(new Chess_Pawn().SetTeam(ChessTeam.White), new Vector2Int(4, 1));
+        board.AddPiece(new Chess_Pawn().SetTeam(ChessTeam.White), new Vector2Int(5, 1));
+        board.AddPiece(new Chess_Pawn().SetTeam(ChessTeam.White), new Vector2Int(6, 1));
+        board.AddPiece(new Chess_Pawn().SetTeam(ChessTeam.White), new Vector2Int(7, 1));
+        board.AddPiece(new Chess_King().SetTeam(ChessTeam.White), new Vector2Int(4, 0));
+        board.AddPiece(new Chess_Queen().SetTeam(ChessTeam.White), new Vector2Int(3, 0));
+        board.AddPiece(new Chess_Bishop().SetTeam(ChessTeam.White), new Vector2Int(2, 0));
+        board.AddPiece(new Chess_Bishop().SetTeam(ChessTeam.White), new Vector2Int(5, 0));
+        board.AddPiece(new Chess_Knight().SetTeam(ChessTeam.White), new Vector2Int(1, 0));
+        board.AddPiece(new Chess_Knight().SetTeam(ChessTeam.White), new Vector2Int(6, 0));
+        board.AddPiece(new Chess_Rook().SetTeam(ChessTeam.White), new Vector2Int(0, 0));
+        board.AddPiece(new Chess_Rook().SetTeam(ChessTeam.White), new Vector2Int(7, 0));
+
+        board.AddPiece(new Chess_Pawn().SetTeam(ChessTeam.Black), new Vector2Int(0, 6));
+        board.AddPiece(new Chess_Pawn().SetTeam(ChessTeam.Black), new Vector2Int(1, 6));
+        board.AddPiece(new Chess_Pawn().SetTeam(ChessTeam.Black), new Vector2Int(2, 6));
+        board.AddPiece(new Chess_Pawn().SetTeam(ChessTeam.Black), new Vector2Int(3, 6));
+        board.AddPiece(new Chess_Pawn().SetTeam(ChessTeam.Black), new Vector2Int(4, 6));
+        board.AddPiece(new Chess_Pawn().SetTeam(ChessTeam.Black), new Vector2Int(5, 6));
+        board.AddPiece(new Chess_Pawn().SetTeam(ChessTeam.Black), new Vector2Int(6, 6));
+        board.AddPiece(new Chess_Pawn().SetTeam(ChessTeam.Black), new Vector2Int(7, 6));
+        board.AddPiece(new Chess_King().SetTeam(ChessTeam.Black), new Vector2Int(4, 7));
+        board.AddPiece(new Chess_Queen().SetTeam(ChessTeam.Black), new Vector2Int(3, 7));
+        board.AddPiece(new Chess_Bishop().SetTeam(ChessTeam.Black), new Vector2Int(2, 7));
+        board.AddPiece(new Chess_Bishop().SetTeam(ChessTeam.Black), new Vector2Int(5, 7));
+        board.AddPiece(new Chess_Knight().SetTeam(ChessTeam.Black), new Vector2Int(1, 7));
+        board.AddPiece(new Chess_Knight().SetTeam(ChessTeam.Black), new Vector2Int(6, 7));
+        board.AddPiece(new Chess_Rook().SetTeam(ChessTeam.Black), new Vector2Int(0, 7));
+        board.AddPiece(new Chess_Rook().SetTeam(ChessTeam.Black), new Vector2Int(7, 7));
+
+        return board;
+    }
+
+
     public enum ChessTeam
     {
         White,
@@ -15,22 +57,71 @@ public class ChessEngine
     }
 }
 
-public class BoardState
+public abstract class BoardState
 {
-    public List<ChessPieceBase> CurrentPieces = new List<ChessPieceBase>();
-    public List<MultiRef<int, string>> History = new List<MultiRef<int, string>>();
+    public List<ChessPieceBase> CurrentPieces = new();
+    public List<MultiRef<int, string>> History = new();
     public int CurrentTurn = 0;
+    public ChessTeam CurrentTeam = ChessTeam.White;
+    public bool IsGameUnderCheck = false;
+    private List<ChessPieceBase> ChangedThisTurn = new();
+
+    public void StartGame()
+    {
+        CurrentTurn = 0;
+        CurrentTeam = ChessTeam.Black;
+        AdvanceTurn();
+    }
+
+    public void AdvanceTurn()
+    {
+        foreach (var a in ChangedThisTurn)
+        {
+            a.CalculateValids(this);
+            RecordHistory(a);
+        }
+        ChangedThisTurn.Clear();
+        switch (CurrentTeam)
+        {
+            case ChessTeam.White:
+                CurrentTeam = ChessTeam.Black;
+                break;
+            case ChessTeam.Black:
+                CurrentTeam = ChessTeam.White;
+                break;
+            case ChessTeam.Red:
+                CurrentTeam = ChessTeam.Blue;
+                break;
+            case ChessTeam.Blue:
+                CurrentTeam = ChessTeam.Yellow;
+                break;
+            case ChessTeam.Yellow:
+                CurrentTeam = ChessTeam.Green;
+                break;
+            case ChessTeam.Green:
+                CurrentTeam = ChessTeam.Red;
+                break;
+
+        }
+        CurrentTurn++;
+        IsGameUnderCheck = IsTeamInCheck(CurrentTeam).valid;
+    }
+
+
     public void AddPiece(ChessPieceBase nerd, Vector2Int pos)
     {
         nerd.Position = pos;
         nerd.Initialize();
         if (nerd.Name == "<Unset>") throw new System.Exception("Bro forgot to give the piece a name");
-        RecordHistory(nerd);
+        ChangedThisTurn.Add(nerd);
     }
     public void RecordHistory(ChessPieceBase nerd)
     {
         History.Add(new MultiRef<int, string>(CurrentTurn, nerd.ToString()));
     }
+
+    public abstract bool IsWithinBounds(Vector2Int pos);
+
     public bool IsOccupied(Vector2Int pos)
     {
         foreach (var piece in CurrentPieces)
@@ -39,11 +130,7 @@ public class BoardState
         }
         return false;
     }
-    public bool IsGameInCheck()
-    {
-        return false;
-    }
-    public ChessPieceBase GetPieceAtPos(Vector2Int pos)
+    public virtual ChessPieceBase GetPieceAtPos(Vector2Int pos)
     {
         foreach (var piece in CurrentPieces)
         {
@@ -61,13 +148,64 @@ public class BoardState
         AddPiece(newpiece, pos);
 
     }
+    /*public bool WouldMoveCauseCheck(ChessPieceBase piece, Vector2Int movepos)
+    {
+        ChessPieceBase king = null;
+        foreach (var p in CurrentPieces)
+        {
+            if (p.Team != piece.Team && p.Name == "King")
+            {
+                king = p;
+                break;
+            }
+        }
+        if (king != null)
+        {
+            return piece.GetAllValidCaptures(this, movepos).Contains(movepos);
+        }
+        else
+        {
+            Debug.LogError("No king found for team " + piece.Team.ToString());
+        }
+        return false;
+    }*/
+    public (bool valid, ChessPieceBase king) IsTeamInCheck(ChessTeam team)
+    {
+        foreach (var piece in CurrentPieces)
+        {
+            if (piece.Name == "King" && piece.Team == team)
+            {
+                foreach (var p in CurrentPieces)
+                {
+                    if (p.Team != piece.Team && p.CanCaptureAt(this, piece.Position))
+                    {
+                        return (true, piece);
+                    }
+                }
+            }
+        }
+        return (false, null);
+    }
+
+    public bool WouldBeInCheckWithMove(ChessPieceBase piece, Vector2Int movepos)
+    {
+        var originalpos = piece.Position;
+        piece.Position = movepos;
+        var (incheck, _) = IsTeamInCheck(piece.Team);
+        piece.Position = originalpos;
+        return incheck;
+    }
+
     public void MovePiece(ChessPieceBase piece, Vector2Int newpos)
     {
         piece.MoveTo(this, newpos);
+        ChangedThisTurn.Add(piece);
     }
+
     public void CapturePiece(ChessPieceBase piece, ChessPieceBase takes, Vector2Int newpos)
     {
         piece.Capture(this, takes, newpos);
+        ChangedThisTurn.Add(piece);
     }
     public bool CanPieceTakeAtPos(ChessPieceBase piece, Vector2Int pos)
     {
@@ -89,10 +227,21 @@ public abstract class ChessPieceBase
     public Vector2Int Position;
     public ChessTeam Team;
     public List<ChessBoardVector> MoveVectors = new List<ChessBoardVector>();
+    public List<Vector2Int> ValidMoves = new();
+    public List<Vector2Int> ValidCaptures = new();
     /// <summary>
     /// Sets the unique name and move vectors
     /// </summary>
     public abstract void Initialize();
+    private bool calced_valids = false;
+    public void CalculateValids(BoardState state)
+    {
+        calced_valids = false;
+        ValidMoves = GetAllValidMoves(state, Position);
+        ValidCaptures = GetAllValidCaptures(state, Position);
+        calced_valids = true;
+    }
+
 
     public Vector2Int TeamRotation(Vector2Int pos)
     {
@@ -118,6 +267,7 @@ public abstract class ChessPieceBase
 
     public virtual List<Vector2Int> GetAllValidMoves(BoardState state, Vector2Int Position)
     {
+        if (Position == this.Position && calced_valids) return ValidMoves;
         List<Vector2Int> fin = new List<Vector2Int>();
         foreach (ChessBoardVector v in MoveVectors)
         {
@@ -126,7 +276,9 @@ public abstract class ChessPieceBase
             foreach (var p in d)
             {
                 var p2 = Position + TeamRotation(p);
+                if (!state.IsWithinBounds(p2)) break;
                 if (state.IsOccupied(p2)) break;
+                if (state.WouldBeInCheckWithMove(this, p2)) break;
                 if (!fin.Contains(p2)) fin.Add(p2);
             }
         }
@@ -134,6 +286,7 @@ public abstract class ChessPieceBase
     }
     public virtual List<Vector2Int> GetAllValidCaptures(BoardState state, Vector2Int Position)
     {
+        if (Position == this.Position && calced_valids) return ValidCaptures;
         List<Vector2Int> fin = new List<Vector2Int>();
         foreach (ChessBoardVector v in MoveVectors)
         {
@@ -142,6 +295,7 @@ public abstract class ChessPieceBase
             foreach (var p in d)
             {
                 var p2 = Position + TeamRotation(p);
+                if (!state.IsWithinBounds(p2)) break;
                 if (state.CanPieceTakeAtPos(this, p2) && !fin.Contains(p2))
                 {
                     fin.Add(p2);
--- Assets/OcksTools/Systems/Extensions/Chess/NormalChessPieces.cs
+++ Assets/OcksTools/Systems/Extensions/Chess/NormalChessPieces.cs
@@ -1,4 +1,14 @@
 using UnityEngine;
+using static ChessEngine;
+
+public class Chess_DefaultBoard : BoardState
+{
+    public override bool IsWithinBounds(Vector2Int pos)
+    {
+        return pos.x >= 0 && pos.x < 8 && pos.y >= 0 && pos.y < 8;
+    }
+}
+
 
 public class Chess_Pawn : ChessPieceBase
 {
@@ -25,10 +35,88 @@ public class Chess_Pawn : ChessPieceBase
 
     public override bool CanBeCapturedByAt(BoardState state, ChessPieceBase nerd, Vector2Int pos)
     {
-        if (nerd.Name == "Pawn" && has_double_push && MovesMade == 1 && nerd.Team != Team && nerd.TurnLastMovedOn == state.CurrentTurn - 1 && pos == Position + TeamRotation(Vector2Int.down)) return true;
+        if (nerd.Name == "Pawn" && has_double_push && MovesMade == 1 && nerd.Team != Team && pos == Position + TeamRotation(Vector2Int.down)) return true;
         return base.CanBeCapturedByAt(state, nerd, pos);
     }
 }
+
+
+public class Chess_King : ChessPieceBase
+{
+
+    public (int left, int right) GetOffsetForCastle()
+    {
+        switch (Team)
+        {
+            case ChessTeam.White: return (4, 3);
+            case ChessTeam.Black: return (3, 4);
+            default: return (4, 3);
+        }
+    }
+
+
+    public override void Initialize()
+    {
+        Name = "King";
+        MoveVectors.Add(new ChessBoardVector(Vector2Int.up, Vector2Int.up));
+        MoveVectors.Add(new ChessBoardVector(Vector2Int.down, Vector2Int.up));
+        MoveVectors.Add(new ChessBoardVector(Vector2Int.left, Vector2Int.left));
+        MoveVectors.Add(new ChessBoardVector(Vector2Int.right, Vector2Int.right));
+        MoveVectors.Add(new ChessBoardVector(Vector2Int.up + Vector2Int.left, Vector2Int.up));
+        MoveVectors.Add(new ChessBoardVector(Vector2Int.up + Vector2Int.right, Vector2Int.up));
+        MoveVectors.Add(new ChessBoardVector(Vector2Int.down + Vector2Int.left, Vector2Int.up));
+        MoveVectors.Add(new ChessBoardVector(Vector2Int.down + Vector2Int.right, Vector2Int.up));
+    }
+
+    public override bool CanMoveToFrom(BoardState state, Vector2Int pos, Vector2Int Position)
+    {
+        if (Position == this.Position && MovesMade == 0 && !state.IsGameUnderCheck)
+        {
+            var p1 = state.GetPieceAtPos(Position + TeamRotation(Vector2Int.right * GetOffsetForCastle().right));
+            var p2 = state.GetPieceAtPos(Position + TeamRotation(Vector2Int.left * GetOffsetForCastle().left));
+            if (p1 != null && p1.Name == "Rook" && p1.MovesMade == 0 && p1.Team == Team)
+            {
+                MoveVectors[2].Direction = Vector2Int.right * 2;
+            }
+            else
+            {
+                MoveVectors[2].Direction = Vector2Int.right;
+            }
+            if (p2 != null && p2.Name == "Rook" && p2.MovesMade == 0 && p2.Team == Team)
+            {
+                MoveVectors[2].Direction = Vector2Int.left * 2;
+            }
+            else
+            {
+                MoveVectors[2].Direction = Vector2Int.left;
+            }
+        }
+        else
+        {
+            MoveVectors[2].Direction = Vector2Int.right;
+            MoveVectors[2].Direction = Vector2Int.left;
+        }
+        return base.CanMoveToFrom(state, pos, Position);
+    }
+
+    public override bool CanMoveTo(BoardState state, Vector2Int pos)
+    {
+        if (MovesMade == 0)
+        {
+            if (pos == Position + TeamRotation(Vector2Int.right * 2))
+            {
+                state.MovePiece(state.GetPieceAtPos(Position + TeamRotation(Vector2Int.right * GetOffsetForCastle().right)), Position + TeamRotation(Vector2Int.right));
+            }
+            else if (pos == Position + TeamRotation(Vector2Int.left * 2))
+            {
+                state.MovePiece(state.GetPieceAtPos(Position + TeamRotation(Vector2Int.left * GetOffsetForCastle().left)), Position + TeamRotation(Vector2Int.left));
+            }
+        }
+        return base.CanMoveTo(state, pos);
+    }
+
+}
+
 
 public class Chess_Bishop : ChessPieceBase
 {
@@ -70,47 +158,6 @@ public class Chess_Queen : ChessPieceBase
     }
 }
 
-public class Chess_King : ChessPieceBase
-{
-    public override void Initialize()
-    {
-        Name = "King";
-        MoveVectors.Add(new ChessBoardVector(Vector2Int.up, Vector2Int.up));
-        MoveVectors.Add(new ChessBoardVector(Vector2Int.down, Vector2Int.up));
-        MoveVectors.Add(new ChessBoardVector(Vector2Int.left, Vector2Int.left));
-        MoveVectors.Add(new ChessBoardVector(Vector2Int.right, Vector2Int.right));
-        MoveVectors.Add(new ChessBoardVector(Vector2Int.up + Vector2Int.left, Vector2Int.up));
-        MoveVectors.Add(new ChessBoardVector(Vector2Int.up + Vector2Int.right, Vector2Int.up));
-        MoveVectors.Add(new ChessBoardVector(Vector2Int.down + Vector2Int.left, Vector2Int.up));
-        MoveVectors.Add(new ChessBoardVector(Vector2Int.down + Vector2Int.right, Vector2Int.up));
-    }
-
-    public override bool CanMoveToFrom(BoardState state, Vector2Int pos, Vector2Int Position)
-    {
-        if (Position == this.Position && MovesMade == 0)
-        {
-            var p1 = state.GetPieceAtPos(Position + TeamRotation(Vector2Int.right * 3));
-            var p2 = state.GetPieceAtPos(Position + TeamRotation(Vector2Int.left * 4));
-            if (p1 != null && p1.Name == "Rook" && p1.MovesMade == 0)
-            {
-                MoveVectors[2].Direction = Vector2Int.right * 2;
-            }
-            else
-            {
-                MoveVectors[2].Direction = Vector2Int.right;
-            }
-            if (p2 != null && p2.Name == "Rook" && p2.MovesMade == 0)
-            {
-                MoveVectors[2].Direction = Vector2Int.left * 2;
-            }
-            else
-            {
-                MoveVectors[2].Direction = Vector2Int.left;
-            }
-        }
-        return base.CanMoveToFrom(state, pos, Position);
-    }
-}
 
 public class Chess_Knight : ChessPieceBase
 {
