56630dfbad5462fe3daaa43f755daee87c5162b3
Ocks <milomgenova@gmail.com>
2026-02-06 13:21:49 -0500

    sd

--- Assets/OcksTools/Systems/SingleInstance.cs
+++ Assets/OcksTools/Systems/SingleInstance.cs
@@ -8,9 +8,17 @@ public abstract class SingleInstance<T> : MonoBehaviour where T : SingleInstance
         private set;
     }
     private void SetInstance(T t) { Instance = t; }
+    /// <summary>
+    /// When overridden, object will be opted out of "DontDestroyOnLoad"
+    /// </summary>
+    public virtual void DestroyOnLoad()
+    {
+        if (transform.parent == null) DontDestroyOnLoad(gameObject);
+    }
     public void Awake()
     {
         SetInstance((T)this);
+        DestroyOnLoad();
         Awake2();
     }
     public virtual void Awake2() { }
