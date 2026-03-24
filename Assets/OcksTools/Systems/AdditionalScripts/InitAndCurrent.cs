b1cbf4fe63e5581a0736357795f41d466324e6ca
Ocks <milomgenova@gmail.com>
2026-03-24 17:03:48 -0400

    a

--- Assets/OcksTools/Systems/AdditionalScripts/InitAndCurrent.cs
+++ Assets/OcksTools/Systems/AdditionalScripts/InitAndCurrent.cs
@@ -13,4 +13,16 @@ public class ResettableValue<T>
     public void SetToInitial() { Current = Initial; }
     public void Reset() => SetToInitial();
     public static implicit operator T(ResettableValue<T> a) { return a.Get(); }
+
+    public void FromString(string a)
+    {
+        var p = a.Split(":");
+        Initial = p[0].StringToObject<T>();
+        Current = p[1].StringToObject<T>();
+    }
+
+    public override string ToString()
+    {
+        return Initial.ToString() + ":" + Current.ToString();
+    }
 }
