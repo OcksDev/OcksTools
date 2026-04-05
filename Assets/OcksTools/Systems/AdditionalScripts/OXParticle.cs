646076025dbd4ed35b8a9962dd8e334c8c97ed1b
Ocks <milomgenova@gmail.com>
2026-04-05 03:33:32 -0400

    a

--- Assets/OcksTools/Systems/AdditionalScripts/OXParticle.cs
+++ Assets/OcksTools/Systems/AdditionalScripts/OXParticle.cs
@@ -1,18 +1,48 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class OXParticle : MonoBehaviour
 {
-    public ParticleSystem ParticleSystem;
+    public List<ParticleSystem> ParticleSystems = new();
+    public ParticleStartType StartType = ParticleStartType.All;
     public ResettableValue<float> LifeTimer = new(2);
     public bool LiveForever = false;
     [HideInInspector]
     public bool HasCustomPlayback = false;
+    public enum ParticleStartType
+    {
+        All,
+        First,
+        Last,
+        None,
+        Random,
+    }
+
     private void Awake()
     {
         OnAwake();
         LifeTimer.Reset();
-        if (ParticleSystem != null) ParticleSystem.Play();
+
+        switch (StartType)
+        {
+            case ParticleStartType.All:
+                foreach (var ps in ParticleSystems) ps.Play();
+                break;
+            case ParticleStartType.First:
+                ParticleSystems[0].Play();
+                break;
+            case ParticleStartType.Last:
+                ParticleSystems[ParticleSystems.Count - 1].Play();
+                break;
+            case ParticleStartType.None:
+                break;
+            case ParticleStartType.Random:
+                ParticleSystems[Random.Range(0, ParticleSystems.Count)].Play();
+                break;
+
+        }
+
         if (HasCustomPlayback) StartCoroutine(Playback());
     }
     private void FixedUpdate()
