a318ebabac3ff20af82d56ee8071eba1c92bce84
Ocks <milomgenova@gmail.com>
2026-08-13 14:33:25 -0400

    a

--- Assets/OcksTools/Systems/OXSceneManager.cs
+++ Assets/OcksTools/Systems/OXSceneManager.cs
@@ -1,10 +1,15 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using UnityEngine;
 using UnityEngine.SceneManagement;
-
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 public class OXSceneManager : SingleInstance<OXSceneManager>
 {
-    public Dictionary<string, SceneData> Datas = new Dictionary<string, SceneData>();
+    public List<MultiRef<string, SceneField>> StaticNameOverrides = new();
+    public Dictionary<string, SceneData> Datas = new();
+    public Dictionary<string, string> StaticNames = new();
     public override void Awake2()
     {
         var d = SceneManager.sceneCount;
@@ -19,9 +24,14 @@ public class OXSceneManager : SingleInstance<OXSceneManager>
             }
             x.IsCurrentlyLoaded.SetValue(true);
         }
+        foreach (var s in StaticNameOverrides)
+        {
+            StaticNames[s.a] = s.b.Name;
+        }
     }
     public SceneData GetScene(string a)
     {
+        if (StaticNames.TryGetValue(a, out var x)) a = x;
         if (!Datas.ContainsKey(a))
         {
             Datas[a] = new SceneData(a);
@@ -29,13 +39,13 @@ public class OXSceneManager : SingleInstance<OXSceneManager>
         return Datas[a];
     }
     public bool IsSceneActive(string a) => GetScene(a).IsCurrentlyActive;
-    public SceneData LoadSceneInstant(string a, bool keep_others = false)
+    public SceneData LoadSceneInstant(string scenename, bool keep_others = false)
     {
-        var s = GetScene(a);
+        var s = GetScene(scenename);
         if (s.IsCurrentlyLoading) return s;
         if (s.IsCurrentlyLoaded) return s;
         if (s.IsCurrentlyActive) return s;
-        SceneManager.LoadScene(a, keep_others ? LoadSceneMode.Additive : LoadSceneMode.Single);
+        SceneManager.LoadScene(s.Name, keep_others ? LoadSceneMode.Additive : LoadSceneMode.Single);
         s.IsCurrentlyLoaded.SetValue(true);
         s.IsCurrentlyActive.SetValue(true);
         foreach (SceneData data in Datas.Values)
@@ -45,15 +55,19 @@ public class OXSceneManager : SingleInstance<OXSceneManager>
         }
         return s;
     }
-
-    public async Task<SceneData> LoadSceneBackground(string a, bool keep_others = false, bool active_when_done = false, bool async_unload = true)
+    public SceneData LoadSceneInstant(SceneField scene, bool keep_others = false)
     {
-        var s = GetScene(a);
+        return LoadSceneInstant(scene.Name, keep_others);
+    }
+
+    public async Task<SceneData> LoadSceneBackground(string scenename, bool keep_others = false, bool active_when_done = false, bool async_unload = true)
+    {
+        var s = GetScene(scenename);
         if (s.IsCurrentlyLoading) return s;
         if (s.IsCurrentlyLoaded) return s;
         if (s.IsCurrentlyActive) return s;
         s.IsCurrentlyLoading.SetValue(true);
-        var loadOp = SceneManager.LoadSceneAsync(a, keep_others ? LoadSceneMode.Additive : LoadSceneMode.Single);
+        var loadOp = SceneManager.LoadSceneAsync(s.Name, keep_others ? LoadSceneMode.Additive : LoadSceneMode.Single);
         while (!loadOp.isDone)
         {
             await Task.Yield(); // wait one frame
@@ -63,16 +77,21 @@ public class OXSceneManager : SingleInstance<OXSceneManager>
         if (active_when_done)
         {
             await Task.Yield();
-            SceneManager.SetActiveScene(SceneManager.GetSceneByName(a));
+            SceneManager.SetActiveScene(SceneManager.GetSceneByName(s.Name));
             s.IsCurrentlyActive.SetValue(true);
             foreach (SceneData data in Datas.Values)
             {
                 if (data == s) continue;
                 data.IsCurrentlyActive.SetValue(false);
-                SceneManager.UnloadSceneAsync(SceneManager.GetSceneByName(data.Name));
+                _ = SceneManager.UnloadSceneAsync(SceneManager.GetSceneByName(data.Name));
             }
         }
         return s;
+    }
+
+    public async Task<SceneData> LoadSceneBackground(SceneField scene, bool keep_others = false, bool active_when_done = false, bool async_unload = true)
+    {
+        return await LoadSceneBackground(scene.Name, keep_others, active_when_done, async_unload);
     }
 }
 
@@ -89,3 +108,47 @@ public class SceneData
         Name = a;
     }
 }
+
+[System.Serializable]
+public struct SceneField : ISerializationCallbackReceiver
+{
+#if UNITY_EDITOR
+    public UnityEditor.SceneAsset Scene;
+#endif
+    [HideInInspector]
+    public string InternalName;
+    public string Name
+    {
+        get
+        {
+#if UNITY_EDITOR
+            return Scene.name;
+#else
+            return InternalName;
+#endif
+        }
+    }
+
+    public void OnBeforeSerialize()
+    {
+#if UNITY_EDITOR
+        if (Scene != null)
+        {
+            InternalName = Scene.name;
+        }
+#endif
+    }
+
+    public void OnAfterDeserialize()
+    {
+        // nyothing needed hewe, nya
+    }
+}
+
+
+#if UNITY_EDITOR
+[CustomPropertyDrawer(typeof(SceneField))]
+public class FuckassSceneDrawer : AutoCompressedInspectorWithName
+{
+}
+#endif
