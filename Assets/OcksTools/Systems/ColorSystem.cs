6a1ec05d8942c273272304ca18fc27d2842245ae
Ocks <milomgenova@gmail.com>
2026-08-19 02:28:51 -0400

    d

--- Assets/OcksTools/Systems/ColorSystem.cs
+++ Assets/OcksTools/Systems/ColorSystem.cs
@@ -9,11 +9,13 @@ using UnityEditor;
 public class ColorSystem : SingleInstance<ColorSystem>
 {
     public CompileableDictionary<HDRColor> HDRColors;
+    public CompileableDictionary<FakeHDRColor> FakeHDRColors;
     public CompileableDictionary<Color> Colors;
     public CompileableDictionary<Material> Materials;
     public override void Awake2()
     {
         HDRColors.Compile();
+        FakeHDRColors.Compile();
         Colors.Compile();
         Materials.Compile();
     }
@@ -21,7 +23,13 @@ public class ColorSystem : SingleInstance<ColorSystem>
     {
         if (Colors.TryGetValue(name, out var c)) return c;
         if (HDRColors.TryGetValue(name, out var hc)) return hc.color;
+        if (FakeHDRColors.TryGetValue(name, out var hc2)) return hc2.color.SetIntensity(hc2.intensity);
         return Color.pink;
+    }
+    public Material GetMaterial(string name)
+    {
+        if (Materials.TryGetValue(name, out var c)) return c;
+        return null;
     }
 }
 
@@ -32,9 +40,39 @@ public struct HDRColor
     public Color color;
 }
 
+[System.Serializable]
+public struct FakeHDRColor
+{
+    public Color color;
+    public float intensity;
+}
+
 #if UNITY_EDITOR
 [CustomPropertyDrawer(typeof(HDRColor))]
-public class FuckassDrawer : AutoCompressedInspector
+public class FuckassDrawer : AutoCompressedSideBySideInspector
+{
+}
+[CustomPropertyDrawer(typeof(FakeHDRColor))]
+public class Fuckass2Drawer : AutoCompressedSideBySideInspector
 {
 }
 #endif
+
+public static class ColorUtils
+{
+    public static Color SetIntensity(this Color a, float intensity)
+    {
+        intensity -= GetIntensity(a);
+        float factor = Mathf.Pow(2, intensity);
+        return new Color(a.r * factor, a.g * factor, a.b * factor);
+    }
+
+    public static float GetIntensity(this Color a)
+    {
+        var maxColorComponent = a.maxColorComponent;
+        if (maxColorComponent <= 0f) return 0f;
+        var scaleFactor = k_MaxByteForOverexposedColor / maxColorComponent;
+        return Mathf.Log(255f / scaleFactor) / Mathf.Log(2f);
+    }
+    private const byte k_MaxByteForOverexposedColor = 191; //internal Unity const
+}
